a92c43f4106bf129ed0aa7639179992481cdbbf0
23427074 <77039165+23427074@users.noreply.github.com>
2021-05-09 19:31:51 +0800

    Update heap_sort.c

--- heap_sort.c
+++ heap_sort.c
@@ -16,37 +16,38 @@ int main()
     ifile = fopen("dataset1.txt", "r");
     int *number = malloc(sizeof(int)*MAX);
     for(int i=0;i<MAX;i++)
-    {
-    	fscanf(ifile, "%d", &number[i]);
+    {	
+    	fscanf(ifile, "%d", &*(number+i));
     }
     gettimeofday(&start,NULL);
     heapSortInt(number, MAX);
     gettimeofday(&end,NULL);
     t = (double)((end.tv_sec*1000000+end.tv_usec)-(start.tv_sec*1000000+start.tv_usec))/1000000.0;
     printf("Heap sort Int time : %lf(s)\n",t);
+    free(number);
     fclose(ifile);
-
+    
     FILE *sfile = NULL;
     sfile = fopen("dataset2.txt", "r");
     char **str = malloc(sizeof(char*)*MAX);
     for(int y=0;y<MAX;y++)
     {
-    	*(str+y) = malloc(sizeof(char)*MAX);
+    	*(str+y) = malloc(sizeof(char)*101);
     }
     for(int i=0;i<MAX;i++)
-    {
-    	fscanf(sfile, "%s", str[i]);
+    {	
+    	fscanf(sfile, "%s", *(str+i));
     }
     gettimeofday(&start,NULL);
     heapSortString(str, MAX);
     gettimeofday(&end,NULL);
     t = (double)((end.tv_sec*1000000+end.tv_usec)-(start.tv_sec*1000000+start.tv_usec))/1000000.0;
-    printf("Heap sort String time : %lf(s)\n",t);
-    for(int y=0;y<MAX;y++)
+    /*for(int y=0;y<MAX;y++)
     {
-    	printf("%s\n", str[y]);
-    }
-
+    	printf("%s\n", *(str+y));
+    }*/
+    printf("Heap sort String time : %lf(s)\n",t);
+    free(str);
     fclose(sfile);
     return 0;
 }

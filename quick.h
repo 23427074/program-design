28f7af2b3a53fe22d8a5b55b92142f0efa3ce5e5
23427074 <77039165+23427074@users.noreply.github.com>
2021-05-09 19:35:26 +0800

    Update quick.h

--- quick.h
+++ quick.h
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef QUICK_H
+#define QUICK_H
 
 #define SWAPInt(x,y) {int t; t = x; x = y; y = t;}
 
 
 void SWAPString(char *x, char *y)
 {
-    char tmp[101];
+    char *tmp = malloc(sizeof(char)*101);
     strcpy(tmp, x);
     strcpy(x,y);
     strcpy(y,tmp);
+    free(tmp);
 }
 
-int partitionInt(int number[], int left, int right)
+int partitionInt(int *number, int left, int right)
 {
     int i = left - 1;
     int j;
     for(j = left; j < right; j++)
     {
-        if(number[j] <= number[right])
+        if(*(number+j) <= *(number+right))
         {
             i++;
-            SWAPInt(number[i], number[j]);
+            SWAPInt(*(number+i), *(number+j));
         }
     }
-    SWAPInt(number[i+1], number[right]);
+    SWAPInt(*(number+i+1), *(number+right));
     return i+1;
 }
 
@@ -37,17 +38,17 @@ int partitionString(char **str, int left, int right)
     int j;
     for(j = left; j < right; j++)
     {
-        if(strcmp(str[j],str[right])<=0)
+        if(strcmp(*(str+j),*(str+right))<=0)
         {
             i++;
-            SWAPString(str[i], str[j]);
+            SWAPString(*(str+i), *(str+j));
         }
     }
-    SWAPString(str[i+1], str[right]);
+    SWAPString(*(str+i+1), *(str+right));
     return i+1;
 }
 
-void quickSortInt(int number[], int left, int right)
+void quickSortInt(int *number, int left, int right)
 {
     if(left < right)
     {
